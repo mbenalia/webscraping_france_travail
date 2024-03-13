88d0a186f3585d9197872599cdbde194e326bfe4
Ryan Homawoo <ryan.homawoo69@gmail.com>
2024-03-13 09:44:52 +0100

    ajout fonction nombre_offre

--- ws_ft.py
+++ ws_ft.py
@@ -9,7 +9,7 @@ import sqlite3
 
 
 def build_url(limite_basse, limite_haute,recherche='data',partenaires="true",):
- 
+
     base_url = "https://candidat.francetravail.fr/offres/recherche"
     params = {
       "motsCles": recherche,
@@ -20,3 +20,22 @@ def build_url(limite_basse, limite_haute,recherche='data',partenaires="true",):
     }
     url = base_url + "?" + "&".join(f"{key}={value}" for key, value in params.items())
     return url
+
+def get_nombre_offres():
+    #fonction à faire évoluer en prenant en compte l'url variable
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    url='https://candidat.francetravail.fr/offres/recherche?motsCles=data&offresPartenaires=true&rayon=10&tri=0'
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        h1_element = soup.find('h1', class_='title')
+        h1_text = h1_element.get_text()
+        nombre_offres = re.search(r'\d+', h1_text).group()
+        return int(nombre_offres)
+    else:
+        print("Erreur lors de la récupération de la page : HTTP", response.status_code)
+        return None
